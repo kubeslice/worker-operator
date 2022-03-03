2521d8db4f888f92504b805fa2b932f17b8be94d
Jayadeep KM <jayadeep@aveshasystems.com>
2022-03-03 06:58:56 +0100

    sample e2e test
    Signed-off-by: Jayadeep KM <jayadeep@aveshasystems.com>

--- e2e/spoke/slice_controller_test.go
+++ e2e/spoke/slice_controller_test.go
@@ -1,14 +1,29 @@
 package spoke_test
 
 import (
+	meshv1beta1 "bitbucket.org/realtimeai/kubeslice-operator/api/v1beta1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("SliceController", func() {
 
 	Context("With a Slice CR created", func() {
-		Expect(true).To(BeTrue())
+		It("Should create the slice", func() {
+
+			slice := &meshv1beta1.Slice{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-slice",
+					Namespace: "kubeslice-system",
+				},
+				Spec: meshv1beta1.SliceSpec{},
+			}
+
+			Expect(k8sClient.Create(ctx, slice)).Should(Succeed())
+
+		})
+
 	})
 
 })
--- e2e/spoke/spoke_suite_test.go
+++ e2e/spoke/spoke_suite_test.go
@@ -7,6 +7,8 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -32,6 +34,8 @@ var testEnv *envtest.Environment
 var ctx context.Context
 var cancel context.CancelFunc
 
+const CONTROL_PLANE_NS = "kubeslice-system"
+
 var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 	ctx, cancel = context.WithCancel(context.TODO())
@@ -54,6 +58,14 @@ var _ = BeforeSuite(func() {
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
+
+	// Create control plane namespace
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: CONTROL_PLANE_NS,
+		},
+	}
+	Expect(k8sClient.Create(ctx, ns)).Should(Succeed())
 
 	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
 		Scheme: scheme.Scheme,
