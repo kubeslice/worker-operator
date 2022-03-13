acf189990426e70962d5b56a3a58f9fb7ca6852c
Jayadeep KM <jayadeep@aveshasystems.com>
2022-03-13 08:00:37 +0100

    make slice specific manifests
    Signed-off-by: Jayadeep KM <jayadeep@aveshasystems.com>

--- internal/manifest/egress.go
+++ internal/manifest/egress.go
@@ -11,58 +11,22 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// Install istio ingress gw resources on the given cluster in a slice
+// Install istio egress gw resources on the given cluster in a slice
 // Resources:
 //  deployment (adds annotations to add the ingress pod to the slice)
 //  serviceaccount
 //  role
 //  rolebinding
 //  service (type clusterip)
-func InstallIngress(ctx context.Context, c client.Client, slice string) error {
-	// TODO make the objects unique per slice by adding slice name
-
+func InstallEgress(ctx context.Context, c client.Client, slice string) error {
 	deploy := &appsv1.Deployment{}
-	err := NewManifest("../../files/ingress/ingress-deploy.json", slice).Parse(deploy)
-	if err != nil {
-		return err
-	}
-
-	// Add the deployment to slice
-	deploy.Labels["slice"] = slice
-	deploy.Annotations = map[string]string{
-		"avesha.io/slice": slice,
-	}
-
-	svc := &corev1.Service{}
-	err = NewManifest("../../files/ingress/ingress-svc.json", slice).Parse(svc)
-	if err != nil {
-		return err
-	}
-
-	role := &rbacv1.Role{}
-	err = NewManifest("../../files/ingress/ingress-role.json", slice).Parse(role)
-	if err != nil {
-		return err
-	}
-
-	sa := &corev1.ServiceAccount{}
-	err = NewManifest("../../files/ingress/ingress-sa.json", slice).Parse(sa)
-	if err != nil {
-		return err
-	}
-
-	rb := &rbacv1.RoleBinding{}
-	err = NewManifest("../../files/ingress/ingress-rolebinding.json", slice).Parse(rb)
+	err := NewManifest("../../files/egress/egress-deploy.json", slice).Parse(deploy)
 	if err != nil {
 		return err
 	}
 
 	objects := []client.Object{
 		deploy,
-		svc,
-		role,
-		sa,
-		rb,
 	}
 
 	for _, o := range objects {
@@ -81,7 +45,7 @@ func InstallIngress(ctx context.Context, c client.Client, slice string) error {
 //  role
 //  rolebinding
 //  service
-func UninstallIngress(ctx context.Context, c client.Client, slice string) error {
+func UninstallEgress(ctx context.Context, c client.Client, slice string) error {
 	// TODO objects should be unique to slice
 
 	log.Info("deleting EW ingress gw for the slice", "slice", slice)
