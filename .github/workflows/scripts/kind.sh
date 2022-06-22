a771a28bec80bd5ec7057c841db416a480ba04db
saivenkatesh <saivenkatesh@aveshasystems.com>
2022-06-22 17:29:58 +0530

    checking the kind clusters with new bash

--- .github/workflows/scripts/kind.sh
+++ .github/workflows/scripts/kind.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-# Create kind clusters
+# Create kind controller cluster
 echo kind create cluster --name kind-controller --config .github/workflows/scripts/kind-controller.yaml
 kind create cluster --name kind-controller --config .github/workflows/scripts/kind-controller.yaml
+
+# Create kind worker-1 cluster
+echo kind create cluster --name kind-worker-1 --config .github/workflows/scripts/kind-worker-1.yaml
+kind create cluster --name kind-worker-1 --config .github/workflows/scripts/kind-worker-1.yaml
+
+# Create kind worker-2 cluster
+echo kind create cluster --name kind-worker-2 --config .github/workflows/scripts/kind-worker-2.yaml
+kind create cluster --name kind-worker-2 --config .github/workflows/scripts/kind-worker-2.yaml
