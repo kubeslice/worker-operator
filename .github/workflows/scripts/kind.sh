c55e642b8088bb3ab5303b3bada4a05212a2dbd3
saivenkatesh <saivenkatesh@aveshasystems.com>
2022-06-22 17:25:25 +0530

    checking the kind clusters with new bash

--- .github/workflows/scripts/kind.sh
+++ .github/workflows/scripts/kind.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-
-# Setup kind multicluster
-HUB="controller"
-SPOKES=["worker-1" "worker-2"]
-PREFIX="kind-"
-
-CLUSTERS=$HUB
-CLUSTERS+=${SPOKES[*]}
-
 # Create kind clusters
-echo kind create cluster --name $HUB --config hub-cluster.yaml
-kind create cluster --name $HUB --config hub-cluster.yaml
-
-echo Create the Worker clusters
-for CLUSTER in ${SPOKES[@]}; do
-    echo kind create cluster --name $CLUSTER --config spoke-cluster.yaml
-    kind create cluster --name $CLUSTER --config spoke-cluster.yaml
-    # Make sure the cluster context exists
-    kubectl cluster-info --context $PREFIX$CLUSTER
-done
+echo kind create cluster --name $HUB --config kind-controller.yaml
+kind create cluster --name controller --config kind-controller.yaml
