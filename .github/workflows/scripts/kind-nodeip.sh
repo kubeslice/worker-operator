6332ca32e4b8a8679ebfaf6e3b200989734b171f
saivenkatesh <saivenkatesh@aveshasystems.com>
2022-07-18 13:10:33 +0530

    making change to the e2e validation actions

--- .github/workflows/scripts/kind-nodeip.sh
+++ .github/workflows/scripts/kind-nodeip.sh
@@ -5,5 +5,4 @@ HOST_IP=$(hostname -i)
 echo $HOST_IP
 
 sed -i "s/<NEW_IP>/${HOST_IP}/g" .github/workflows/scripts/kind-controller.yaml
-sed -i "s/<NEW_IP>/${HOST_IP}/g" .github/workflows/scripts/kind-worker-1.yaml
-sed -i "s/<NEW_IP>/${HOST_IP}/g" .github/workflows/scripts/kind-worker-2.yaml
+sed -i "s/<NEW_IP>/${HOST_IP}/g" .github/workflows/scripts/kind-worker.yaml
--- .github/workflows/scripts/kind.sh
+++ .github/workflows/scripts/kind.sh
@@ -3,10 +3,6 @@
 echo kind create cluster --name controller --config .github/workflows/scripts/kind-controller.yaml
 kind create cluster --name controller --config .github/workflows/scripts/kind-controller.yaml
 
-# Create kind worker-1 cluster
-echo kind create cluster --name worker-1 --config .github/workflows/scripts/kind-worker-1.yaml
-kind create cluster --name worker-1 --config .github/workflows/scripts/kind-worker-1.yaml
-
-# Create kind worker-2 cluster
-echo kind create cluster --name worker-2 --config .github/workflows/scripts/kind-worker-2.yaml
-kind create cluster --name worker-2 --config .github/workflows/scripts/kind-worker-2.yaml
+# Create kind worker cluster
+echo kind create cluster --name worker --config .github/workflows/scripts/kind-worker.yaml
+kind create cluster --name worker --config .github/workflows/scripts/kind-worker.yaml
