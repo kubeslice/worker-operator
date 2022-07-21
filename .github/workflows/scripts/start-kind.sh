5b68e21ab07cbbf638e208780dc6c1fe6abfcb8f
saivenkatesh <saivenkatesh@aveshasystems.com>
2022-07-21 12:50:31 +0530

    changign the e2e startkind

--- .github/workflows/scripts/start-kind.sh
+++ .github/workflows/scripts/start-kind.sh
@@ -1,44 +1,41 @@
 #!/bin/bash
 
-HOST_IP=$(hostname -i)
-
 # Create controller kind cluster if not present
 if [ ! $(kind get clusters | grep controller) ];then
   kind create cluster --name controller --config .github/workflows/scripts/cluster.yaml --image kindest/node:v1.22.7
-  ip=$(docker inspect controller-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
+  ip=$(docker inspect controller-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress') 
 
   # Replace loopback IP with docker ip
-  kind get kubeconfig --name controller | sed "s/127.0.0.1.*/$HOST_IP:6443/g" > /home/runner/.kube/kind1.yaml
+  kind get kubeconfig --name controller | sed "s/127.0.0.1.*/$ip:6443/g" > /home/runner/.kube/kind1.yaml
 fi
 
 # Create worker1 kind cluster if not present
-if [ ! $(kind get clusters | grep worker1) ];then
+# Create worker1 kind cluster if not present
+if [ ! $(kind get clusters | grep worker) ];then
   kind create cluster --name worker --config .github/workflows/scripts/cluster.yaml --image kindest/node:v1.22.7
   ip=$(docker inspect worker-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
 
   # Replace loopback IP with docker ip
-  kind get kubeconfig --name worker | sed "s/127.0.0.1.*/$HOST_IP:7443/g" > /home/runner/.kube/kind2.yaml
+  kind get kubeconfig --name worker | sed "s/127.0.0.1.*/$ip:6443/g" > /home/runner/.kube/kind2.yaml
 fi
 
-KUBECONFIG=/home/runner/.kube/kind1.yaml:/home/runner/.kube/kind2.yaml kubectl config view --raw  > /home/runner/.kube/kinde2e.yaml
+KUBECONFIG=/home/runner/.kube/kind1.yaml:/home/runner/.kube/kind2.yaml kubectl config view --raw  > /home/runner/.kube/kind.yaml
 
 if [ ! -f profile/kind.yaml ];then
   # Provide correct IP in kind profile, since worker operator cannot detect internal IP as nodeIp
   ip1=$(docker inspect controller-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
 
-  ip2=$(docker inspect worker-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
+  ip2=$(docker inspect worker1-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
 
-pwd
-
-  cat > kind.yaml << EOF
-Kubeconfig: kinde2e.yaml
+  cat > profile/kind.yaml << EOF
+Kubeconfig: kind.yaml
 ControllerCluster:
   Context: kind-controller
 WorkerClusters:
 - Context: kind-controller
-  NodeIP: ${HOST_IP}
-- Context: kind-worker
-  NodeIP: ${HOST_IP}
+  NodeIP: ${ip1}
+- Context: kind-worker1
+  NodeIP: ${ip2}
 WorkerChartOptions:
   SetStrValues:
     "operator.image": "worker-operator"
