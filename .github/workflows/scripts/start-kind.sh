f9fc7e43b7dc1b9f36a3cac94c0cb87b97d93ff6
saivenkatesh <saivenkatesh@aveshasystems.com>
2022-07-21 12:29:25 +0530

    changign the e2e script-startkind-change

--- .github/workflows/scripts/start-kind.sh
+++ .github/workflows/scripts/start-kind.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+HOST_IP=$(hostname -i)
+
 # Create controller kind cluster if not present
 if [ ! $(kind get clusters | grep controller) ];then
-  kind create cluster --name controller --config .github/workflows/scripts/kind-controller.yaml --image kindest/node:v1.22.7
+  kind create cluster --name controller --config .github/workflows/scripts/cluster.yaml --image kindest/node:v1.22.7
   ip=$(docker inspect controller-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
 
   # Replace loopback IP with docker ip
-  kind get kubeconfig --name controller | sed "s/127.0.0.1.*/$ip:6443/g" > /home/runner/.kube/kind1.yaml
+  kind get kubeconfig --name controller | sed "s/127.0.0.1.*/$HOST_IP:6443/g" > /home/runner/.kube/kind1.yaml
 fi
 
 # Create worker1 kind cluster if not present
 if [ ! $(kind get clusters | grep worker1) ];then
-  kind create cluster --name worker --config .github/workflows/scripts/kind-worker.yaml --image kindest/node:v1.22.7
+  kind create cluster --name worker --config .github/workflows/scripts/cluster.yaml --image kindest/node:v1.22.7
   ip=$(docker inspect worker-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
 
   # Replace loopback IP with docker ip
-  kind get kubeconfig --name worker | sed "s/127.0.0.1.*/$ip:6443/g" > /home/runner/.kube/kind2.yaml
+  kind get kubeconfig --name worker | sed "s/127.0.0.1.*/$HOST_IP:7443/g" > /home/runner/.kube/kind2.yaml
 fi
 
 KUBECONFIG=/home/runner/.kube/kind1.yaml:/home/runner/.kube/kind2.yaml kubectl config view --raw  > /home/runner/.kube/kinde2e.yaml
@@ -34,9 +36,9 @@ ControllerCluster:
   Context: kind-controller
 WorkerClusters:
 - Context: kind-controller
-  NodeIP: ${ip1}
+  NodeIP: ${HOST_IP}
 - Context: kind-worker
-  NodeIP: ${ip2}
+  NodeIP: ${HOST_IP}
 WorkerChartOptions:
   SetStrValues:
     "operator.image": "worker-operator"
