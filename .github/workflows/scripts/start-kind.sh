cffbc45f73fdda78ebdf2956ecf47ef48c8c3bb9
saivenkatesh <saivenkatesh@aveshasystems.com>
2022-07-22 14:10:18 +0530

    adding changes to the start kind

--- .github/workflows/scripts/start-kind.sh
+++ .github/workflows/scripts/start-kind.sh
@@ -2,28 +2,28 @@
 
 HOST_IP=$(hostname -i)
 
-sed -i "s/<NEW_IP>/${HOST_IP}/g" .github/workflows/scripts/kind-controller.yaml
-sed -i "s/<NEW_IP>/${HOST_IP}/g" .github/workflows/scripts/kind-worker.yaml
+#sed -i "s/<NEW_IP>/${HOST_IP}/g" .github/workflows/scripts/kind-controller.yaml
+#sed -i "s/<NEW_IP>/${HOST_IP}/g" .github/workflows/scripts/kind-worker.yaml
 
 # Create controller kind cluster if not present
 if [ ! $(kind get clusters | grep controller) ];then
-  kind create cluster --name controller --config .github/workflows/scripts/kind-controller.yaml --image kindest/node:v1.22.7
+  kind create cluster --name controller --config .github/workflows/scripts/cluster.yaml --image kindest/node:v1.22.7
 #  ip=$(docker inspect controller-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress') 
 #  echo $ip
   # Replace loopback IP with docker ip
-  kind get kubeconfig --name kind-controller | sed "s/127.0.0.1.*/$HOST_IP:6443/g" > /home/runner/.kube/kind1.yaml
+  kind get kubeconfig --name controller | sed "s/127.0.0.1.*/$HOST_IP:6443/g" > /home/runner/.kube/kind1.yaml
   cat /home/runner/.kube/kind1.yaml
 fi
 
 # Create worker1 kind cluster if not present
 # Create worker1 kind cluster if not present
 if [ ! $(kind get clusters | grep worker) ];then
-  kind create cluster --name worker --config .github/workflows/scripts/kind-worker.yaml --image kindest/node:v1.22.7
+  kind create cluster --name worker --config .github/workflows/scripts/cluster.yaml --image kindest/node:v1.22.7
 #  ip=$(docker inspect worker-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
 #  echo $ip
   # Replace loopback IP with docker ip
-  kind get kubeconfig --name kind-worker | sed "s/127.0.0.1.*/$HOST_IP:7443/g" > /home/runner/.kube/kind2.yaml
-  /home/runner/.kube/kind2.yaml
+  kind get kubeconfig --name worker | sed "s/127.0.0.1.*/$HOST_IP:7443/g" > /home/runner/.kube/kind2.yaml
+  cat /home/runner/.kube/kind2.yaml
 fi
 
 KUBECONFIG=/home/runner/.kube/kind1.yaml:/home/runner/.kube/kind2.yaml kubectl config view --raw  > /home/runner/.kube/kinde2e.yaml
@@ -33,9 +33,9 @@ if [ ! -f profile/kind.yaml ];then
   # Provide correct IP in kind profile, since worker operator cannot detect internal IP as nodeIp
   docker ps
   kind get clusters
-  $HOST_IP1=$(docker inspect controller-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
+  HOST_IP1=$(docker inspect controller-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
   echo $HOST_IP1
-  $HOST_IP2=$(docker inspect worker-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
+  HOST_IP2=$(docker inspect worker-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
   echo $HOST_IP2
 
   cat > kind.yaml << EOF
