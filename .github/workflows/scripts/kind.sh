0e80c3c46271ccedb8400bf9566b9e5527e22e16
saivenkatesh1909 <99859187+saivenkatesh1909@users.noreply.github.com>
2022-07-26 09:25:37 +0530

    testing gh-pages (#67)
    adding gh-pages for the workflow

--- .github/workflows/scripts/kind.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Create kind controller cluster
-echo kind create cluster --name controller --config .github/workflows/scripts/kind-controller.yaml
-kind create cluster --name controller --config .github/workflows/scripts/kind-controller.yaml
-
-# Create kind worker cluster
-echo kind create cluster --name worker --config .github/workflows/scripts/kind-worker.yaml
-kind create cluster --name worker --config .github/workflows/scripts/kind-worker.yaml
