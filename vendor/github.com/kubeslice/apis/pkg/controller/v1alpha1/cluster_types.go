5eb0d97d5e8b3560ec25fd1215278307f230ec3d
rahulsawra98 <104886535+rahulsawra98@users.noreply.github.com>
2022-08-19 11:50:50 +0530

    fix(AM-4841): fix for geolocation (#82)
    * fix(AM-4841): fix for lat/long overriding and clustertype

--- vendor/github.com/kubeslice/apis/pkg/controller/v1alpha1/cluster_types.go
+++ vendor/github.com/kubeslice/apis/pkg/controller/v1alpha1/cluster_types.go
@@ -59,6 +59,10 @@ type GeoLocation struct {
 	CloudProvider string `json:"cloudProvider,omitempty"`
 	//CloudRegion is the region of the cloud
 	CloudRegion string `json:"cloudRegion,omitempty"`
+	//Latitude is the latitude of the cluster
+	Latitude string `json:"latitude,omitempty"`
+	//Longitude is the longitude of the cluster
+	Longitude string `json:"longitude,omitempty"`
 }
 
 // Monitoring defines the field of ClusterSpec
