32a8351681dda147e581fda22e8fea9e4c60907b
kon3m <aakash.dokka@aveshasystems.com>
2022-08-24 16:37:57 +0530

    added intial test case

--- tests/spoke/gateway_redundancy_test.go
+++ tests/spoke/gateway_redundancy_test.go
@@ -1,32 +1,37 @@
 package spoke_test
 
 import (
-	"fmt"
+	"context"
 	"os"
 	"time"
 
 	hubv1alpha1 "github.com/kubeslice/apis/pkg/controller/v1alpha1"
+	kubeslicev1beta1 "github.com/kubeslice/worker-operator/api/v1beta1"
 	clusterpkg "github.com/kubeslice/worker-operator/pkg/cluster"
 	hub "github.com/kubeslice/worker-operator/pkg/hub/hubclient"
+	nsmv1alpha1 "github.com/networkservicemesh/networkservicemesh/k8s/pkg/apis/networkservice/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	apps "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/retry"
 )
 
-var _ = Describe("Gateway redundancy test scenario", func() {
-	// var sliceGw *kubeslicev1beta1.SliceGateway
-	// var createdSliceGw *kubeslicev1beta1.SliceGateway
+var _ = FDescribe("Gateway redundancy test scenario", func() {
+	var sliceGw *kubeslicev1beta1.SliceGateway
+	var createdSliceGw *kubeslicev1beta1.SliceGateway
 	var cluster *hubv1alpha1.Cluster
-	// var slice *kubeslicev1beta1.Slice
+	var slice, createdSlice *kubeslicev1beta1.Slice
 	// var createdSlice *kubeslicev1beta1.Slice
-	// var svc *corev1.Service
+	var svc *corev1.Service
+	var nsmconfig *corev1.ConfigMap
 	var ns *corev1.Namespace
-	var node1, node2 *corev1.Node
-	// var vl3ServiceEndpoint *nsmv1alpha1.NetworkServiceEndpoint
-
+	var node1, node2 *corev1.Node //node2
+	var vl3ServiceEndpoint *nsmv1alpha1.NetworkServiceEndpoint
+	var createdDeploy *apps.Deployment
 	Context("With Redundant GW pods", func() {
 		BeforeEach(func() {
 			//create 2 kubeslice GW nodes
@@ -40,7 +45,9 @@ var _ = Describe("Gateway redundancy test scenario", func() {
 						"topology.kubeslice.io/gateway": "0",
 					},
 				},
-				Spec: corev1.NodeSpec{},
+				Spec: corev1.NodeSpec{
+					ProviderID: "gce://demo",
+				},
 				Status: corev1.NodeStatus{
 					Addresses: []corev1.NodeAddress{
 						{
@@ -96,58 +103,66 @@ var _ = Describe("Gateway redundancy test scenario", func() {
 			}
 			Expect(k8sClient.Create(ctx, cluster)).Should(Succeed())
 
-			// sliceGw = &kubeslicev1beta1.SliceGateway{
-			// 	ObjectMeta: metav1.ObjectMeta{
-			// 		Name:      "test-slicegw-pod",
-			// 		Namespace: CONTROL_PLANE_NS,
-			// 	},
-			// 	Spec: kubeslicev1beta1.SliceGatewaySpec{
-			// 		SliceName: "test-slice-gw",
-			// 	},
-			// }
+			sliceGw = &kubeslicev1beta1.SliceGateway{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-slicegw-pod",
+					Namespace: CONTROL_PLANE_NS,
+				},
+				Spec: kubeslicev1beta1.SliceGatewaySpec{
+					SliceName: "test-slice-gw",
+				},
+			}
 
-			// svc = &corev1.Service{
-			// 	ObjectMeta: metav1.ObjectMeta{
-			// 		Name:      "kubeslice-dns",
-			// 		Namespace: CONTROL_PLANE_NS,
-			// 	},
-			// 	Spec: corev1.ServiceSpec{
-			// 		ClusterIP: "10.0.0.20",
-			// 		Ports: []corev1.ServicePort{{
-			// 			Port: 52,
-			// 		}},
-			// 	},
-			// }
+			svc = &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "kubeslice-dns",
+					Namespace: CONTROL_PLANE_NS,
+				},
+				Spec: corev1.ServiceSpec{
+					ClusterIP: "10.0.0.20",
+					Ports: []corev1.ServicePort{{
+						Port: 52,
+					}},
+				},
+			}
 
-			// slice = &kubeslicev1beta1.Slice{
-			// 	ObjectMeta: metav1.ObjectMeta{
-			// 		Name:      "test-slice-gw",
-			// 		Namespace: CONTROL_PLANE_NS,
-			// 	},
-			// 	Spec: kubeslicev1beta1.SliceSpec{},
-			// }
+			slice = &kubeslicev1beta1.Slice{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-slice-gw",
+					Namespace: CONTROL_PLANE_NS,
+				},
+				Spec: kubeslicev1beta1.SliceSpec{},
+			}
 
-			// vl3ServiceEndpoint = &nsmv1alpha1.NetworkServiceEndpoint{
-			// 	TypeMeta: metav1.TypeMeta{
-			// 		APIVersion: "networkservicemesh.io/v1alpha1",
-			// 		Kind:       "NetworkServiceEndpoint",
-			// 	},
-			// 	ObjectMeta: metav1.ObjectMeta{
-			// 		GenerateName: "vl3-service-" + "test-slice-gw",
-			// 		Namespace:    "kubeslice-system",
-			// 		Labels: map[string]string{
-			// 			"app":                "vl3-nse-" + "test-slice-gw",
-			// 			"networkservicename": "vl3-service-" + "test-slice-gw",
-			// 		},
-			// 	},
-			// 	Spec: nsmv1alpha1.NetworkServiceEndpointSpec{
-			// 		NetworkServiceName: "vl3-service-" + "test-slice-gw",
-			// 		Payload:            "IP",
-			// 		NsmName:            "test-node",
-			// 	},
-			// }
+			vl3ServiceEndpoint = &nsmv1alpha1.NetworkServiceEndpoint{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "networkservicemesh.io/v1alpha1",
+					Kind:       "NetworkServiceEndpoint",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					GenerateName: "vl3-service-" + "test-slice-gw",
+					Namespace:    "kubeslice-system",
+					Labels: map[string]string{
+						"app":                "vl3-nse-" + "test-slice-gw",
+						"networkservicename": "vl3-service-" + "test-slice-gw",
+					},
+				},
+				Spec: nsmv1alpha1.NetworkServiceEndpointSpec{
+					NetworkServiceName: "vl3-service-" + "test-slice-gw",
+					Payload:            "IP",
+					NsmName:            "test-node",
+				},
+			}
+			nsmconfig = configMap("nsm-config", "kubeslice-system", `
+prefixes:
+- 192.168.0.0/16
+- 10.96.0.0/12`)
+			err = k8sClient.Get(ctx, types.NamespacedName{Name: nsmconfig.Name, Namespace: nsmconfig.Namespace}, nsmconfig)
+			if errors.IsNotFound(err) {
+				Expect(k8sClient.Create(ctx, nsmconfig)).Should(Succeed())
+			}
 			// createdSlice = &kubeslicev1beta1.Slice{}
-			// createdSliceGw = &kubeslicev1beta1.SliceGateway{}
+			createdSliceGw = &kubeslicev1beta1.SliceGateway{}
 
 			DeferCleanup(func() {
 				Eventually(func() bool {
@@ -168,22 +183,66 @@ var _ = Describe("Gateway redundancy test scenario", func() {
 		It("Should update cluster CR with List of Node IPs", func() {
 			os.Setenv("CLUSTER_NAME", cluster.Name)
 			os.Setenv("HUB_PROJECT_NAMESPACE", PROJECT_NS)
-			nodeIpList, err := clusterpkg.GetNodeIP(k8sClient)
+			ctx := context.Background()
+			nodeIP, err := clusterpkg.GetNodeIP(k8sClient)
 			Expect(err).To(BeNil())
-
-			fmt.Println(nodeIpList, "------------------------gw")
-
-			err = hub.PostClusterInfoToHub(ctx, k8sClient, k8sClient, "cluster-gw", "kubeslice-cisco", nodeIpList)
+			//post GeoLocation and other metadata to cluster CR on Hub cluster
+			err = hub.PostClusterInfoToHub(ctx, k8sClient, k8sClient, "cluster-gw", "kubeslice-cisco", nodeIP)
 			Expect(err).To(BeNil())
 			//get the cluster object
 			Eventually(func() bool {
 				err := k8sClient.Get(ctx, types.NamespacedName{Name: cluster.Name, Namespace: cluster.Namespace}, cluster)
 				return err == nil
 			}, time.Second*10, time.Millisecond*250).Should(BeTrue())
-			Expect(cluster.Spec.NodeIPs).Should(Equal(nodeIpList))
+			// start the reconcilers
+			Expect(k8sClient.Create(ctx, svc)).Should(Succeed())
+			Expect(k8sClient.Create(ctx, slice)).Should(Succeed())
+			Expect(k8sClient.Create(ctx, vl3ServiceEndpoint)).Should(Succeed())
+			Expect(k8sClient.Create(ctx, sliceGw)).Should(Succeed())
+			slicekey := types.NamespacedName{Name: "test-slice-gw", Namespace: CONTROL_PLANE_NS}
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, slicekey, createdSlice)
+				return err == nil
+			}, time.Second*10, time.Millisecond*250).Should(BeTrue())
+			slicegwkey := types.NamespacedName{Name: "test-slicegw-pod", Namespace: CONTROL_PLANE_NS}
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, slicegwkey, createdSliceGw)
+				return err == nil
+			}, time.Second*10, time.Millisecond*250).Should(BeTrue())
+
+			createdSliceGw.Status.Config.SliceGatewayHostType = "Server"
+			err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
+				err := k8sClient.Get(ctx, slicegwkey, createdSliceGw)
+				if err != nil {
+					return err
+				}
+				createdSliceGw.Status.Config.SliceGatewayHostType = "Server"
+				err = k8sClient.Status().Update(ctx, createdSliceGw)
+				if err != nil {
+					return err
+				}
+				return nil
+			})
+			Expect(err).To(BeNil())
+			deploykey := types.NamespacedName{Name: "test-slicegw-pod", Namespace: CONTROL_PLANE_NS}
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, deploykey, createdDeploy)
+				return err == nil
+			}, time.Second*10, time.Millisecond*250).Should(BeTrue())
+			//create another kubeslice node
+			// Expect(k8sClient.Create(ctx, node2)).Should(Succeed())
+			// delete the node whose IP was selected to replicate node failure
+			Expect(k8sClient.Delete(ctx, node1)).Should(Succeed())
+			// verify if new node IP is updated on cluster CR
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, types.NamespacedName{Name: cluster.Name, Namespace: cluster.Namespace}, cluster)
+				return err == nil
+			}, time.Second*60, time.Millisecond*250).Should(BeTrue())
+			Expect(cluster.Spec.NodeIPs).Should(Equal(nodeIP))
+
 		})
 
-		// It("Should Provision two client GW pods on different nodes")
+		//It("Should Provision two client GW pods on different nodes")
 
 		// It("Should Provision two server GW pods on different nodes")
 
