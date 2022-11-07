0f3f0804aacf42401b0132065fb670fc90b4fd09
kon3m <aakash.dokka@aveshasystems.com>
2022-11-07 18:38:13 +0530

    feat(): updating pos labels
    Signed-off-by: kon3m <aakash.dokka@aveshasystems.com>

--- controllers/slicegateway/slicegateway.go
+++ controllers/slicegateway/slicegateway.go
@@ -1067,6 +1067,7 @@ func (r *SliceGwReconciler) findAndRemovePodFromNode(ctx context.Context) error 
 	log.Info("removing label from pods", "pod", newestPod)
 	delete(newestPod.Labels, controllers.PodTypeSelectorLabelKey)
 	newestPod.Labels["kubeslice.io/pod-type"] = "toBeDeleted"
+	r.Client.Update(ctx, newestPod)
 	return nil
 
 }
