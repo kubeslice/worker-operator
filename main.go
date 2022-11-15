6bd417c2a03c3648e01a7b22a5c8a002cac2bc4c
rahulsawra98 <rahul.sawra@aveshasystems.com>
2022-11-15 12:54:03 +0530

    fix(AM-XXXX): test on rancher

--- main.go
+++ main.go
@@ -282,10 +282,10 @@ func main() {
 	}
 
 	//post dashboard creds cluster CR on Hub cluster
-	err = hub.PostDashboardCredsToHub(ctx, clientForHubMgr, hubClient)
-	if err != nil {
-		setupLog.With("error", err).Error("could not post Dasboard Creds to Hub")
-	}
+	// err = hub.PostDashboardCredsToHub(ctx, clientForHubMgr, hubClient)
+	// if err != nil {
+	// 	setupLog.With("error", err).Error("could not post Dasboard Creds to Hub")
+	// }
 
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctx); err != nil {
