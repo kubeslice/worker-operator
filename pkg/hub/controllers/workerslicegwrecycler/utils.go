c79ab8f1ba14ee8daf8670fcfffb3fe753f20219
rahulsawra98 <rahul.sawra@aveshasystems.com>
2022-12-01 19:54:39 +0530

    minor fix

--- pkg/hub/controllers/workerslicegwrecycler/utils.go
+++ pkg/hub/controllers/workerslicegwrecycler/utils.go
@@ -146,7 +146,7 @@ func (r *Reconciler) update_routing_table(e *fsm.Event) error {
 	listOptions := []client.ListOption{
 		client.MatchingLabels(labels),
 	}
-	err := r.MeshClient.List(ctx, &podList, listOptions...)
+	err = r.MeshClient.List(ctx, &podList, listOptions...)
 	if err != nil {
 		return err
 	}
