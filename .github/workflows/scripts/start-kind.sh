cf4d41d1aa4c62327395c3fd64f498944f35e1d9
Nishant Singh <89241026+NishantSingh10@users.noreply.github.com>
2022-12-20 08:23:01 +0530

    Update start-kind.sh

--- .github/workflows/scripts/start-kind.sh
+++ .github/workflows/scripts/start-kind.sh
@@ -6,6 +6,7 @@ git clone https://github.com/ahmetb/kubectx /opt/kubectx
 ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 
+
 # Create controller kind cluster if not present
 if [ ! $(kind get clusters | grep controller) ];then
   kind create cluster --name controller --config .github/workflows/scripts/cluster.yaml --image kindest/node:v1.23.12
