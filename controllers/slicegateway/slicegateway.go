f849ee9b19714e21edc6c54f92ea92a99366ec21
Rahul-D78 <rahul.kumar@aveshasystems.com>
2023-02-09 15:27:41 +0530

    fix(): fix wrong lable
    Signed-off-by: Rahul-D78 <rahul.kumar@aveshasystems.com>

--- controllers/slicegateway/slicegateway.go
+++ controllers/slicegateway/slicegateway.go
@@ -1065,7 +1065,7 @@ func getPodAntiAffinity(slice string) *corev1.PodAntiAffinity {
 func (r *SliceGwReconciler) getNewestPod(slicegw *kubeslicev1beta1.SliceGateway) (*corev1.Pod, error) {
 	PodList := corev1.PodList{}
 	labels := map[string]string{"kubeslice.io/pod-type": "slicegateway", controllers.ApplicationNamespaceSelectorLabelKey: slicegw.Spec.SliceName,
-		"kubeslice.io/slicegw": slicegw.Name}
+		"kubeslice.io/slice-gw": slicegw.Name}
 	listOptions := []client.ListOption{
 		client.MatchingLabels(labels),
 	}
