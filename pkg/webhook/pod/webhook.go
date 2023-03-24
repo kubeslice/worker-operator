768df40ca9a2bc607eac42c21306f2841517f97d
Rahul-D78 <rahul.kumar@aveshasystems.com>
2023-03-24 21:00:40 +0530

    checking for rc
    Signed-off-by: Rahul-D78 <rahul.kumar@aveshasystems.com>

--- pkg/webhook/pod/webhook.go
+++ pkg/webhook/pod/webhook.go
@@ -61,7 +61,7 @@ type WebhookServer struct {
 }
 
 func (wh *WebhookServer) Handle(ctx context.Context, req admission.Request) admission.Response {
-	log.Info("webhook operation", "rahul", req.Operation)
+	log.Info("debugging webhook", "###rahul", req.Operation)
 	if req.Kind.Kind == "Pod" {
 		pod := &corev1.Pod{}
 		err := wh.decoder.Decode(req, pod)
