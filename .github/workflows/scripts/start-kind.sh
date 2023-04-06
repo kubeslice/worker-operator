22c3777a81e1c3d15709ba08cf3f64e18cdf2412
Nishant Singh <89241026+NishantSingh10@users.noreply.github.com>
2023-04-06 13:00:53 +0530

    Update start-kind.sh

--- .github/workflows/scripts/start-kind.sh
+++ .github/workflows/scripts/start-kind.sh
@@ -187,11 +187,11 @@ IstioDChartOptions:
   Password: ${chartpassword}
   Namespace: "istio-system"
 TestSuitesEnabled:
-  EmptySuite: "${EMPTYSUITE}"
-  HubSuite: "${HUBSUITE}"
-  WorkerSuite: "${WORKERSUITE}"
-  IstioSuite: "${ISTIOSUITE}"
-  IntraClusterSuite: "${INTRACLUSTERSUITE}"
+  EmptySuite: ${EMPTYSUITE}
+  HubSuite: ${HUBSUITE}
+  WorkerSuite: ${WORKERSUITE}
+  IstioSuite: ${ISTIOSUITE}
+  IntraClusterSuite: ${INTRACLUSTERSUITE}
 EOF
 
 fi
