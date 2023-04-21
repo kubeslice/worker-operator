d18277748e77e878a6e7dc1ce950b0abd8d7a689
YachikaRalhan <yachika.ralhan@aveshasystems.com>
2023-04-21 16:59:55 +0530

    using kubectl patch and few fixes
    Signed-off-by: YachikaRalhan <yachika.ralhan@aveshasystems.com>

--- pkg/hub/controllers/cluster/deregister.go
+++ pkg/hub/controllers/cluster/deregister.go
@@ -151,71 +151,79 @@ func constructConfigMap() *corev1.ConfigMap {
 		},
 		Data: map[string]string{
 			"kubeslice-cleanup.sh": `
-				#!/bin/bash
-				updateControllerCluster() {
-				curl -i --location --request PATCH $HubEndpoint/apis/controller.kubeslice.io/v1alpha1/namespaces/$ProjectNamespace/clusters/$ClusterName --header 'Content-Type: application/merge-patch+json' --header "Authorization: Bearer $TOKEN" --cacert /ca.crt --data-raw '{
-					"spec": {
-						"networkInterface": "'"$1"'"
-					}
-				}'
-				}
-				
-				deleteKubeSliceCRDs() {
-					kubectl delete crd serviceexports.networking.kubeslice.io
-					kubectl delete crd serviceimports.networking.kubeslice.io
-					kubectl delete crd slice.networking.kubeslice.io
-					kubectl delete crd slicegateways.networking.kubeslice.io
-					kubectl delete crd slicenodeaffinities.networking.kubeslice.io
-					kubectl delete crd sliceresourcequotas.networking.kubeslice.io
-					kubectl delete crd slicerolebindings.networking.kubeslice.io
-				}
-		
-				deleteNamespace(){
-					kubectl delete namespace kubeslice-system
-				}
-		
-				SECRET=kubeslice-hub
-				TOKEN=$(kubectl get secret ${SECRET} -o json | jq -Mr '.data.token' | base64 -d)
-				kubectl get secret ${SECRET} -o json | jq -Mr '.data["ca.crt"]' | base64 -d > /ca.crt
-				if helm uninstall kubeslice-worker --namespace kubeslice-system --dry-run
-				then
-					updateControllerCluster "DeregisterSuccess"
+			#!/bin/bash
+
+			updateControllerClusterStatus() {
+			  kubectl patch clusters.controller.kubeslice.io $CLUSTER_NAME -n $PROJECT_NAMESPACE --type=merge --subresource status --patch "{\"status\": {\"registrationStatus\": \"$1\"}}"
+			}
+	
+			removeFinalizer() {
+			  kubectl get -o yaml clusters.controller.kubeslice.io $CLUSTER_NAME -n $PROJECT_NAMESPACE --token $TOKEN  --certificate-authority /ca.crt --server $HUB_ENDPOINT > ./cluster.yaml
+			  sed -i '/finalizers:/,/^[^ ]/ s/ *- worker.kubeslice.io\/deregister-finalizer//' cluster.yaml
+			  kubectl patch clusters.controller.kubeslice.io $CLUSTER_NAME -n $PROJECT_NAMESPACE --type=merge  --patch-file cluster.yaml --token $TOKEN  --certificate-authority /ca.crt --server $HUB_ENDPOINT
+			}
+	
+			deleteKubeSliceCRDs() {
+			  kubectl delete crd serviceexports.networking.kubeslice.io --ignore-not-found
+			  kubectl delete crd serviceimports.networking.kubeslice.io --ignore-not-found
+			  kubectl delete crd slices.networking.kubeslice.io --ignore-not-found
+			  kubectl delete crd slicegateways.networking.kubeslice.io --ignore-not-found
+			  kubectl delete crd slicenodeaffinities.networking.kubeslice.io --ignore-not-found
+			  kubectl delete crd sliceresourcequotas.networking.kubeslice.io --ignore-not-found
+			  kubectl delete crd slicerolebindings.networking.kubeslice.io --ignore-not-found
+			}
+	
+			deleteNamespace(){
+			  kubectl delete namespace kubeslice-system
+			}
+	
+			SECRET=kubeslice-hub
+			TOKEN=$(kubectl get secret ${SECRET} -o json | jq -Mr '.data.token' | base64 -d)
+			kubectl get secret ${SECRET} -o json | jq -Mr '.data["ca.crt"]' | base64 -d > /ca.crt
+			# get the worker release name
+			workerRelease=$(helm list --output json -n kubeslice-system | jq -r '.[] | select(.chart | startswith("kubeslice-worker-")).name')
+			echo workerRelease $workerRelease
+			if helm uninstall $workerRelease --namespace kubeslice-system
+			then
+				# updateControllerClusterStatus "DeregisterSuccess"
+				removeFinalizer
+				# delete crds
+				deleteKubeSliceCRDs
+				# delete namespace
+				deleteNamespace
+			else
+			  if helm uninstall $workerRelease --namespace kubeslice-system --no-hooks --dry-run
+			  then 
+				# deleting nsm mutatingwebhookconfig
+					WH=$(kubectl get pods -l app=admission-webhook-k8s  -o jsonpath='{.items[*].metadata.name}')
+					kubectl delete mutatingwebhookconfiguration --ignore-not-found ${WH}
+	
+				# removing finalizers from spire and kubeslice-system namespaces
+					NAMESPACES=(spire kubeslice-system)
+					for ns in ${NAMESPACES[@]}
+					do
+					  kubectl get ns $ns -o name  
+					  if [[ $? -eq 1 ]]; then
+						  echo "$ns namespace was deleted successfully"
+						  continue
+					  fi
+					  echo "finding and removing spiffeids in namespace $ns ..."
+					  for item in $(kubectl get spiffeid.spiffeid.spiffe.io -n $ns -o name); do
+						echo "removing item $item"
+						kubectl patch $item -p '{"metadata":{"finalizers":null}}' --type=merge -n $ns
+						kubectl delete $item --ignore-not-found -n $ns
+					  done
+					done
+					updateControllerClusterStatus "DeregisterSuccess"
+					removeFinalizer
 					# delete crds
 					deleteKubeSliceCRDs
 					# delete namespace
 					deleteNamespace
-				else
-					if helm uninstall kubeslice-worker --namespace kubeslice-system --no-hooks --dry-run
-					then 
-						# deleting nsm mutatingwebhookconfig
-							WH=$(kubectl get pods -l app=admission-webhook-k8s  -o jsonpath='{.items[*].metadata.name}')
-							kubectl delete mutatingwebhookconfiguration --ignore-not-found ${WH}
-			
-						# removing finalizers from spire and kubeslice-system namespaces
-							NAMESPACES=(spire kubeslice-system)
-							for ns in ${NAMESPACES[@]}
-							do
-							kubectl get ns $ns -o name  
-							if [[ $? -eq 1 ]]; then
-								echo "$ns namespace was deleted successfully"
-								continue
-							fi
-							echo "finding and removing spiffeids in namespace $ns ..."
-							for item in $(kubectl get spiffeid.spiffeid.spiffe.io -n $ns -o name); do
-								echo "removing item $item"
-								kubectl patch $item -p '{"metadata":{"finalizers":null}}' --type=merge -n $ns
-								kubectl delete $item --ignore-not-found -n $ns
-							done
-							done
-							updateControllerCluster "DeregisterSuccess"
-							# delete crds
-							deleteKubeSliceCRDs
-							# delete namespace
-							deleteNamespace
-					else 
-						updateControllerCluster "DeregisterFailed"
-					fi
-				fi`,
+			  else 
+				updateControllerClusterStatus "DeregisterFailed"
+			  fi
+			fi`,
 		}}
 	return cm
 }
