3b71ed4fd2576672290fed932f8c4e4429928c50
Rahul-D78 <rahul.kumar@aveshasystems.com>
2023-05-02 22:04:15 +0530

    update latest monitoring framework, log changes in cleanup.sh
    Signed-off-by: Rahul-D78 <rahul.kumar@aveshasystems.com>

--- vendor/github.com/kubeslice/kubeslice-monitoring/pkg/events/events.go
+++ vendor/github.com/kubeslice/kubeslice-monitoring/pkg/events/events.go
@@ -62,11 +62,7 @@ func NewEventRecorder(c client.Writer, s *runtime.Scheme, em map[EventName]*Even
 }
 
 var _ EventRecorder = (*eventRecorder)(nil)
-
-type EventRecorderOptions struct {
 	// Version is the version of the component
-	Version string
-	// Cluster  is the name of the cluster
 	Cluster string
 	// Project is the name of the project
 	Project string
