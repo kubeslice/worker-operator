966c6f65ba2b441f39fa7192893fc8c6dc0dc384
rahulsawra98 <rahul.sawra@aveshasystems.com>
2023-07-05 15:42:41 +0530

    changes in workerslicegwrecycler reconciler and label addition
    Signed-off-by: rahulsawra98 <rahul.sawra@aveshasystems.com>

---
+++ pkg/hub/controllers/workerslicegwrecycler/helpers.go
@@ -0,0 +1,23 @@
+package workerslicegwrecycler
+
+import (
+	"fmt"
+
+	spokev1alpha1 "github.com/kubeslice/apis/pkg/worker/v1alpha1"
+)
+
+func getDeployLabels(workerslicegwrecycler *spokev1alpha1.WorkerSliceGwRecycler) map[string]string {
+	return map[string]string{
+		"kubeslice.io/slicegw":                      workerslicegwrecycler.Spec.SliceGwClient,
+		"kubeslice.io/slicegatewayRedundancyNumber": fmt.Sprint(workerslicegwrecycler.Spec.RedundancyNumber),
+	}
+}
+
+func getPodLabels(workerslicegwrecycler *spokev1alpha1.WorkerSliceGwRecycler, sliceGateway string) map[string]string {
+	return map[string]string{
+		"kubeslice.io/pod-type":                     "slicegateway",
+		"kubeslice.io/slice":                        workerslicegwrecycler.Spec.SliceName,
+		"kubeslice.io/slice-gw":                     sliceGateway,
+		"kubeslice.io/slicegatewayRedundancyNumber": fmt.Sprint(workerslicegwrecycler.Spec.RedundancyNumber),
+	}
+}
