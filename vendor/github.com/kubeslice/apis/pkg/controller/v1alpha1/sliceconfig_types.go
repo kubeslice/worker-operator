6be8ad27d5af20636898a3ee6ede0f2867306bb1
YachikaRalhan <yachika.ralhan@aveshasystems.com>
2023-07-19 11:16:36 +0530

    fix(): updated ctrl apis dependency
    Signed-off-by: YachikaRalhan <yachika.ralhan@aveshasystems.com>

--- vendor/github.com/kubeslice/apis/pkg/controller/v1alpha1/sliceconfig_types.go
+++ vendor/github.com/kubeslice/apis/pkg/controller/v1alpha1/sliceconfig_types.go
@@ -42,8 +42,13 @@ type SliceConfigSpec struct {
 	//+kubebuilder:validation:Minimum=2
 	//+kubebuilder:validation:Maximum=32
 	MaxClusters int `json:"maxClusters"`
+	//+kubebuilder:validation:Minimum=30
+	//+kubebuilder:validation:Maximum=90
+	//+kubebuilder:default:=30
+	RotationInterval int `json:"rotationInterval,omitempty"`
 	// RenewBefore is used for renew now!
-	RenewBefore metav1.Time `json:"renewBefore,omitempty"`
+	RenewBefore *metav1.Time      `json:"renewBefore,omitempty"`
+	VPNConfig   *VPNConfiguration `json:"vpnConfig,omitempty"`
 }
 
 // ExternalGatewayConfig is the configuration for external gateways like 'istio', etc/
@@ -106,6 +111,14 @@ type NamespaceIsolationProfile struct {
 type SliceNamespaceSelection struct {
 	Namespace string   `json:"namespace,omitempty"`
 	Clusters  []string `json:"clusters,omitempty"`
+}
+
+// VPNConfiguration defines the additional (optional) VPN Configuration to customise
+type VPNConfiguration struct {
+	//+kubebuilder:default:=AES-256-CBC
+	//+kubebuilder:validation:Required
+	//+kubebuilder:validation:Enum:=AES-256-CBC;AES-128-CBC
+	Cipher string `json:"cipher"`
 }
 
 type KubesliceEvent struct {
