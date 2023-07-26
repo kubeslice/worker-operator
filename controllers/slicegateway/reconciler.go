c3c4c704abd65750501cee28d01f5f8b91bbedb4
Yachika <36668209+YachikaRalhan@users.noreply.github.com>
2023-07-26 11:22:32 +0530

    Syncing release nainital (#280)
    * fix(): releasing latest changes
    
    * error handling in fsm
    
    Signed-off-by: YachikaRalhan <yachika.ralhan@aveshasystems.com>
    
    * fix(): fsm bug fixes-v1 (#276)
    
    Signed-off-by: rahulsawra98 <rahul.sawra@aveshasystems.com>
    
    * fix(): recycler name as gw pod name (#277)
    
    Signed-off-by: YachikaRalhan <yachika.ralhan@aveshasystems.com>
    
    * fix(): disabled rebalancing (#279)
    
    Signed-off-by: YachikaRalhan <yachika.ralhan@aveshasystems.com>
    
    ---------
    
    Signed-off-by: YachikaRalhan <yachika.ralhan@aveshasystems.com>
    Signed-off-by: rahulsawra98 <rahul.sawra@aveshasystems.com>
    Co-authored-by: Richie Sebastian <104349170+richiesebastian@users.noreply.github.com>
    Co-authored-by: rahulsawra98 <104886535+rahulsawra98@users.noreply.github.com>

--- controllers/slicegateway/reconciler.go
+++ controllers/slicegateway/reconciler.go
@@ -321,30 +321,30 @@ func (r *SliceGwReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		utils.RecordEvent(ctx, r.EventRecorder, sliceGw, slice, ossEvents.EventSliceNetopQoSSyncFailed, controllerName)
 		return ctrl.Result{}, err
 	}
-	if isServer(sliceGw) {
-		toRebalance, err := r.isRebalancingRequired(ctx, sliceGw)
-		if err != nil {
-			log.Error(err, "Unable to rebalace gw pods")
-			utils.RecordEvent(ctx, r.EventRecorder, sliceGw, slice, ossEvents.EventSliceGWRebalancingFailed, controllerName)
-			return ctrl.Result{}, err
-		}
-		log.Info("Rebalancing required?", "toRebalance", toRebalance)
-		if toRebalance {
-			// start FSM for graceful termination of gateway pods
-			// create workerslicegwrecycler on controller
-			newestPod, err := r.getNewestPod(sliceGw)
-			if err != nil {
-				return ctrl.Result{}, err
-			}
-			err = r.WorkerRecyclerClient.TriggerFSM(sliceGw, slice, newestPod, controllerName, sliceGwName+"-"+fmt.Sprint(0), 1)
-			// to maintain consistency with recycling we are creating this CR with zero as suffix in the name
-			if err != nil {
-				// TODO:add an event and log
-				log.Error(err, "Error while recycling gateway pods")
-				return ctrl.Result{}, err
-			}
-		}
-	}
+	// if isServer(sliceGw) {
+	// 	toRebalance, err := r.isRebalancingRequired(ctx, sliceGw)
+	// 	if err != nil {
+	// 		log.Error(err, "Unable to rebalance gw pods")
+	// 		utils.RecordEvent(ctx, r.EventRecorder, sliceGw, slice, ossEvents.EventSliceGWRebalancingFailed, controllerName)
+	// 		return ctrl.Result{}, err
+	// 	}
+	// 	log.Info("Rebalancing required?", "toRebalance", toRebalance)
+	// 	if toRebalance {
+	// 		// start FSM for graceful termination of gateway pods
+	// 		// create workerslicegwrecycler on controller
+	// 		newestPod, err := r.getNewestPod(sliceGw)
+	// 		if err != nil {
+	// 			return ctrl.Result{}, err
+	// 		}
+	// 		err = r.WorkerRecyclerClient.TriggerFSM(sliceGw, slice, newestPod, controllerName, newestPod.Name, 1)
+	// 		// to maintain consistency with recycling we are creating this CR with zero as suffix in the name
+	// 		if err != nil {
+	// 			// TODO:add an event and log
+	// 			log.Error(err, "Error while recycling gateway pods")
+	// 			return ctrl.Result{}, err
+	// 		}
+	// 	}
+	// }
 	return ctrl.Result{Requeue: true}, nil
 }
 
