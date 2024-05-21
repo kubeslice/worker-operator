b3c625e7e5d39191a9465c858055b9d4f71421fd
Rahul Kumar <57394571+Rahul-D78@users.noreply.github.com>
2024-05-21 11:25:07 +0530

    fix(): remove hardcoded worker installer image (#383)
    Signed-off-by: Rahul-D78 <rahul.kumar@aveshasystems.com>

--- pkg/hub/controllers/cluster/vars.go
+++ pkg/hub/controllers/cluster/vars.go
@@ -25,12 +25,13 @@ import (
 )
 
 var (
-	ControlPlaneNamespace = "kubeslice-system"
-	HubSecretSuffix       = "-kubernetes-dashboard"
-	KubeSliceDashboardSA  = "kubeslice-kubernetes-dashboard"
-	ProjectNamespace      = os.Getenv("HUB_PROJECT_NAMESPACE")
-	HubEndpoint           = os.Getenv("HUB_HOST_ENDPOINT")
-	HubTokenFile          = utils.GetEnvOrDefault("HUB_TOKEN_FILE", "/var/run/secrets/kubernetes.io/hub-serviceaccount/token")
-	HubCAFile             = utils.GetEnvOrDefault("HUB_CA_FILE", "/var/run/secrets/kubernetes.io/hub-serviceaccount/ca.crt")
-	ClusterName           = os.Getenv("CLUSTER_NAME")
+	ControlPlaneNamespace          = "kubeslice-system"
+	HubSecretSuffix                = "-kubernetes-dashboard"
+	KubeSliceDashboardSA           = "kubeslice-kubernetes-dashboard"
+	ProjectNamespace               = os.Getenv("HUB_PROJECT_NAMESPACE")
+	HubEndpoint                    = os.Getenv("HUB_HOST_ENDPOINT")
+	HubTokenFile                   = utils.GetEnvOrDefault("HUB_TOKEN_FILE", "/var/run/secrets/kubernetes.io/hub-serviceaccount/token")
+	HubCAFile                      = utils.GetEnvOrDefault("HUB_CA_FILE", "/var/run/secrets/kubernetes.io/hub-serviceaccount/ca.crt")
+	ClusterName                    = os.Getenv("CLUSTER_NAME")
+	WORKER_INSTALLER_DEFAULT_IMAGE = "aveshasystems/worker-installer:1.1.8"
 )
