8767ad0b3bdde85ce395d0a812b67747d2302d69
Mridul Gain <mridulgain@gmail.com>
2024-06-12 12:55:38 +0530

    import fix
    Signed-off-by: Mridul Gain <mridulgain@gmail.com>

--- controllers/slice/reconciler.go
+++ controllers/slice/reconciler.go
@@ -36,6 +36,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
