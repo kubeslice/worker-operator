fbf2deccd1a9fc3e497ec01ae437741ba57fa8af
Priyank Upadhyay <priyank@aveshasystems.com>
2024-10-30 11:31:19 +0530

    fix(): configurable ns exclusion list (#408)
    * fix(): configurable ns exclusion list
    
    Signed-off-by: Priyank Upadhyay <priyank@aveshasystems.com>
    
    * fix(): refactors
    
    Signed-off-by: Priyank Upadhyay <priyank@aveshasystems.com>
    
    ---------
    
    Signed-off-by: Priyank Upadhyay <priyank@aveshasystems.com>

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -1,6 +1,7 @@
 package utils
 
 const (
-	NotApplicable = "NA"
-	EventsVersion = "v1alpha1"
+	NotApplicable     = "NA"
+	EventsVersion     = "v1alpha1"
+	DefaultExcludedNS = "kube-system,default,kubeslice-system,kube-node-lease,kube-public,istio-system"
 )
